90d4ae45c8343ef87613d8490a9ebc3d371e36d3
Udit1806 <usengar8@gmail.com>
2016-07-12 17:53:15 +0530

    Intial Commit

---
+++ app/src/main/java/com/teamtreehouse/musicmachine/MainActivity.java
@@ -0,0 +1,92 @@
+package com.teamtreehouse.musicmachine;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String KEY_SONG = "song";
+    private boolean mBound = false;
+    private PlayerService mPlayerService;
+    private Button mDownloadButton;
+    private Button mPlayButton;
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            mBound = true;
+            PlayerService.LocalBinder localBinder = (PlayerService.LocalBinder) binder;
+            mPlayerService = localBinder.getService();
+            if (mPlayerService.isPlaying()) {
+                mPlayButton.setText("Pause");
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDownloadButton = (Button) findViewById(R.id.downloadButton);
+        mPlayButton = (Button) findViewById(R.id.playButton);
+
+        mDownloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Downloading", Toast.LENGTH_SHORT).show();
+
+                // Send Messages to Handler for processing
+                for (String song : Playlist.songs) {
+                    Intent intent = new Intent(MainActivity.this, DownloadIntentService.class);
+                    intent.putExtra(KEY_SONG, song);
+                    startService(intent);
+                }
+            }
+        });
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBound) {
+                    if (mPlayerService.isPlaying()) {
+                        mPlayerService.pause();
+                        mPlayButton.setText("Play");
+                    } else {
+                        mPlayerService.play();
+                        mPlayButton.setText("Pause");
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, PlayerService.class);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            unbindService(mServiceConnection);
+            mBound = false;
+        }
+    }
+}
---
+++ app/src/main/java/com/teamtreehouse/musicmachine/PlayerService.java
@@ -0,0 +1,59 @@
+package com.teamtreehouse.musicmachine;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class PlayerService extends Service {
+    private static final String TAG = PlayerService.class.getSimpleName();
+    private MediaPlayer mPlayer;
+    private IBinder mBinder = new LocalBinder();
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate");
+        mPlayer = MediaPlayer.create(this, R.raw.jingle);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind");
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "onUnbind");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        mPlayer.release();
+    }
+
+    public class LocalBinder extends Binder {
+        public PlayerService getService() {
+            return PlayerService.this;
+        }
+    }
+
+    // Client Methods
+    public boolean isPlaying() {
+        return mPlayer.isPlaying();
+    }
+
+    public void play() {
+        mPlayer.start();
+    }
+
+    public void pause() {
+        mPlayer.pause();
+    }
+}
